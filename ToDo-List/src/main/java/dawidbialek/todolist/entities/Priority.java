b3dbec4c9fc98d1fcabaa284dba27da69c1113c1
DawidBialek <dd.bialek@gmail.com>
2024-12-05 12:19:32 +0000

    add Priority and Task entity

---
+++ ToDo-List/src/main/java/dawidbialek/todolist/entities/Priority.java
@@ -0,0 +1,7 @@
+package dawidbialek.todolist.entities;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ ToDo-List/src/main/java/dawidbialek/todolist/entities/Task.java
@@ -0,0 +1,38 @@
+package dawidbialek.todolist.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime creationTime;
+
+    private LocalDateTime deadline;
+
+    @NotNull
+    private Priority priority;
+
+}
