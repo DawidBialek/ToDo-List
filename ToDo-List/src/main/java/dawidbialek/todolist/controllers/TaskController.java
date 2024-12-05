90e1851ad00dbb8c2dbf24631a240d9faab13690
DawidBialek <dd.bialek@gmail.com>
2024-12-05 21:21:39 +0000

    add basic task controller

---
+++ ToDo-List/src/main/java/dawidbialek/todolist/controllers/TaskController.java
@@ -0,0 +1,13 @@
+package dawidbialek.todolist.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
