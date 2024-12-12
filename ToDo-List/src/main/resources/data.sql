b26d133058c86b75dda3aad077b45b43be7d8411
DawidBialek <dd.bialek@gmail.com>
2024-12-12 15:57:57 +0000

    remove data.sql and schema.sql

--- ToDo-List/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into task values(1, 'hello', 'something');
-insert into task values(2, 'hi', 'stuff');
--- ToDo-List/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table task
-(
-    id integer not null,
-    title varchar(255) not null,
-    description varchar(255) not null
-);
